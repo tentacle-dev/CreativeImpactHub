f7285bd11ce065b85388403f65de2785437fc317
tentacle-dev <tanush0525@gmail.com>
2025-06-06 15:12:50 -0400

    Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
